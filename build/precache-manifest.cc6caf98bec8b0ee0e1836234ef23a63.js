924d77b41cd8056afc7dbc3233b40c3a547d424a
Michael Martino <Mick666@hotmail.com.au>
2020-07-06 14:56:42 +1000

    Added react build

---
+++ build/precache-manifest.cc6caf98bec8b0ee0e1836234ef23a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6809a134d3eaeaa14cd913a741a1de2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f6f3428cddd966db0d8",
+    "url": "/static/js/2.2902c74c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2902c74c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada037cd7180ca39af6a",
+    "url": "/static/js/main.06e19816.chunk.js"
+  },
+  {
+    "revision": "dc11ffb1a1fcdaebfb6e",
+    "url": "/static/js/runtime-main.15a3d875.js"
+  }
+]);
